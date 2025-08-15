50fce9740e4040e1c211ec63d3a99bfcc61fbf41
timothy joseph <timothyjoseph8580@gmail.com>
2025-08-15 19:50:31 +0300

    solved the import error on some files

--- src/Api/routes/analytics-routes.js
+++ src/Api/routes/analytics-routes.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { getTopProducts, getShopPerformanceSummary, getSalesByStatus } from './controllers/analytics-controller.js';
-import { userAuth } from '../../middleware/verification.js';
+import { getTopProducts, getShopPerformanceSummary, getSalesByStatus } from '../controllers/analytics-controller.js';
+import verifyUser from '../../middleware/verification.js';
 
 const router = express.Router();
 
-router.get('/top-products', userAuth, getTopProducts);
-router.get('/shop-performance-summary', userAuth, getShopPerformanceSummary);
-router.get('/sales-by-status', userAuth, getSalesByStatus);
+router.get('/top-products', verifyUser, getTopProducts);
+router.get('/shop-performance-summary', verifyUser, getShopPerformanceSummary);
+router.get('/sales-by-status', verifyUser, getSalesByStatus);
 
 export default router;
